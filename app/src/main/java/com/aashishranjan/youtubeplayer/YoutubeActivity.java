8602809ef56e43cb94cad48b02790c71799a826c
aashish-swiggy <aashish.ranjan@swiggy.in>
2021-01-16 09:34:54 +0530

    add youtubeActivity and activity_youtube layout

---
+++ app/src/main/java/com/aashishranjan/youtubeplayer/YoutubeActivity.java
@@ -0,0 +1,39 @@
+package com.aashishranjan.youtubeplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout player_layout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+        ConstraintLayout player_layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(player_layout);
+
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        player_layout.addView(player);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
